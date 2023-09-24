6e7a462bf416870de68a12659cd8cfe0220be3af
Knight6767704 <knightsind6767704@gmail.com>
2023-09-24 18:29:20 +0530

    toggle

--- navbar.js
+++ navbar.js
@@ -1,31 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Get references to your navbar and toggle button elements
-  const navbar = document.getElementById('navbar-sticky');
-  const toggleButton = document.getElementById('toggleNavbar');
+const toggleButton = document.getElementById("toggleButton");
+const navbar = document.getElementById("navbar-sticky");
 
-  // Function to close the navbar
-  function closeNavbar() {
-    navbar.classList.remove('md:block');
-    navbar.setAttribute('aria-expanded', 'false');
-    // Add any additional code to hide or close the navbar as needed
-  }
+toggleButton.addEventListener("click", () => {
+    navbar.classList.toggle("hidden");
+});
 
-  // Event listener to close navbar when clicking outside
-  document.addEventListener('click', function (event) {
-    // Check if the click target is not inside the navbar or toggle button
-    if (!navbar.contains(event.target) && event.target !== toggleButton) {
-      closeNavbar();
+document.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target !== toggleButton && !navbar.contains(target)) {
+        navbar.classList.add("hidden");
     }
-  });
-
-  // Event listener to toggle the navbar when the button is clicked
-  toggleButton.addEventListener('click', function () {
-    const expanded = navbar.getAttribute('aria-expanded');
-    if (expanded === 'true') {
-      closeNavbar();
-    } else {
-      navbar.classList.add('md:block');
-      navbar.setAttribute('aria-expanded', 'true');
-    }
-  });
 });
