9852b71f4161b9ad02f4e01ec81c83c8156b33d0
Knight6767704 <knightsind6767704@gmail.com>
2023-09-24 18:49:17 +0530

    finally

--- navbar.js
+++ navbar.js
@@ -1,13 +1,22 @@
 const toggleButton = document.getElementById("toggleButton");
 const navbar = document.getElementById("navbar-sticky");
 
-toggleButton.addEventListener("click", () => {
+function toggleNavbar() {
     navbar.classList.toggle("hidden");
+}
+
+function closeNavbar() {
+    navbar.classList.add("hidden");
+}
+
+toggleButton.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent click propagation to document
+    toggleNavbar();
 });
 
 document.addEventListener("click", (event) => {
     const target = event.target;
     if (target !== toggleButton && !navbar.contains(target)) {
-        navbar.classList.add("hidden");
+        closeNavbar();
     }
 });
